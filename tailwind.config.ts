8641094a7758f85f923a851cc19e42aeed2ce7a2
l-bianchi <illorenzobianchi@proton.me>
2024-07-04 14:32:19 +0200

    Added catppuccin theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,24 @@
 import { type Config } from "tailwindcss";
+import { flavorEntries } from "https://deno.land/x/catppuccin@v1.2.0/mod.ts";
+
+const colors = Object.fromEntries(
+  flavorEntries.map((
+    [flavorName, { colorEntries }],
+  ) => [
+    flavorName,
+    Object.fromEntries(
+      colorEntries.map(([colorName, { hex }]) => [colorName, hex]),
+    ),
+  ]),
+);
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
 } satisfies Config;
